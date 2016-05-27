06cd3db857219cd0bd36a4d79eb6a4d03584513b
Kirill Efimov <kirill89@gmail.com>
2016-05-27 13:43:44 +0300

    tests: improved mixins coverage

--- test/mixins.js
+++ test/mixins.js
@@ -73,7 +73,7 @@ describe('mixins', function () {
   });
 
 
-  it('twitter-player', function (done) {
+  it('twitter-player html5', function (done) {
     let mixin = mixins.find(m => m.id === 'twitter-player');
     let env = {
       wl: { twitter: { player: [ 'allow', 'html5' ] } },
@@ -84,6 +84,24 @@ describe('mixins', function () {
     mixin.fn(env, (err) => {
       assert.deepStrictEqual(env.result.snippets, [
         { href: '/foo', tags: [ 'player', 'twitter', 'responsive', 'html5' ], type: 'text/html', media: {} }
+      ]);
+
+      done(err);
+    });
+  });
+
+
+  it('twitter-player', function (done) {
+    let mixin = mixins.find(m => m.id === 'twitter-player');
+    let env = {
+      wl: { twitter: { player: 'allow' } },
+      data: { meta: [ { name: 'twitter:player:src', value: '/foo' } ] },
+      result: { snippets: [] }
+    };
+
+    mixin.fn(env, (err) => {
+      assert.deepStrictEqual(env.result.snippets, [
+        { href: '/foo', tags: [ 'player', 'twitter', 'responsive' ], media: {} }
       ]);
 
       done(err);
@@ -136,7 +154,7 @@ describe('mixins', function () {
   });
 
 
-  it('oembed-player', function (done) {
+  it('oembed-player html5', function (done) {
     let mixin = mixins.find(m => m.id === 'oembed-player');
     let env = {
       wl: { oembed: { video: 'allow' } },
@@ -151,6 +169,43 @@ describe('mixins', function () {
       assert.deepStrictEqual(env.result.snippets, [
         { href: '/foo', tags: [ 'player', 'oembed', 'responsive', 'html5' ], media: {}, type: 'text/html' }
       ]);
+      done(err);
+    });
+  });
+
+
+  it('oembed-player html', function (done) {
+    let mixin = mixins.find(m => m.id === 'oembed-player');
+    let env = {
+      wl: { oembed: { video: 'allow' } },
+      data: { oembed: {
+        type: 'video',
+        html: '<iframe src="/foo"></iframe>'
+      } },
+      result: { snippets: [] }
+    };
+
+    mixin.fn(env, (err) => {
+      assert.deepStrictEqual(env.result.snippets, [
+        { href: '/foo', tags: [ 'player', 'oembed', 'responsive', 'html5' ], media: {}, type: 'text/html' }
+      ]);
+      done(err);
+    });
+  });
+
+
+  it('oembed-player no href', function (done) {
+    let mixin = mixins.find(m => m.id === 'oembed-player');
+    let env = {
+      wl: { oembed: { video: 'allow' } },
+      data: { oembed: {
+        type: 'video'
+      } },
+      result: { snippets: [] }
+    };
+
+    mixin.fn(env, (err) => {
+      assert.deepStrictEqual(env.result.snippets, []);
       done(err);
     });
   });
@@ -238,6 +293,60 @@ describe('mixins', function () {
   });
 
 
+  it('oembed-rich with size', function (done) {
+    let mixin = mixins.find(m => m.id === 'oembed-rich');
+    let env = {
+      wl: { oembed: { rich: 'allow' } },
+      data: { oembed: {
+        type: 'rich',
+        html: '<iframe src="/foo" width="100" height="200"></iframe>',
+        width: 150,
+        height: 250
+      } },
+      result: { snippets: [] }
+    };
+
+    mixin.fn(env, (err) => {
+      assert.deepStrictEqual(env.result.snippets, [
+        {
+          href: '/foo',
+          tags: [ 'oembed', 'rich' ],
+          media: { width: 150, height: 250 },
+          type: 'text/html',
+          html: ''
+        }
+      ]);
+      done(err);
+    });
+  });
+
+
+  it('oembed-rich no iframe', function (done) {
+    let mixin = mixins.find(m => m.id === 'oembed-rich');
+    let env = {
+      wl: { oembed: { rich: 'allow' } },
+      data: { oembed: {
+        type: 'rich'
+      } },
+      result: { snippets: [] }
+    };
+
+    mixin.fn(env, (err) => {
+      assert.deepStrictEqual(env.result.snippets, [
+        {
+          href: null,
+          tags: [ 'oembed', 'rich' ],
+          media: {},
+          type: 'text/html',
+          /* eslint-disable no-undefined */
+          html: undefined
+        }
+      ]);
+      done(err);
+    });
+  });
+
+
   it('oembed-rich inline', function (done) {
     let mixin = mixins.find(m => m.id === 'oembed-rich');
     let env = {
@@ -268,15 +377,31 @@ describe('mixins', function () {
     let mixin = mixins.find(m => m.id === 'favicon');
     let env = {
       data: { links: { icon: [
-        { type: 'foo', href: 'bar', sizes: '10x15' }
+        { type: 'foo', href: 'bar', sizes: '10x15' },
+        { href: 'baz' }
       ] } },
       result: { snippets: [] }
     };
 
     mixin.fn(env, (err) => {
       assert.deepStrictEqual(env.result.snippets, [
-        { type: 'foo', href: 'bar', tags: [ 'icon' ], media: { width: 10, height: 15 } }
+        { type: 'foo', href: 'bar', tags: [ 'icon' ], media: { width: 10, height: 15 } },
+        { type: 'image', href: 'baz', tags: [ 'icon' ], media: {} }
       ]);
+      done(err);
+    });
+  });
+
+
+  it('favicon no links', function (done) {
+    let mixin = mixins.find(m => m.id === 'favicon');
+    let env = {
+      data: { links: { test: [] } },
+      result: { snippets: [] }
+    };
+
+    mixin.fn(env, (err) => {
+      assert.deepStrictEqual(env.result.snippets, []);
       done(err);
     });
   });
--- test/mixins_after.js
+++ test/mixins_after.js
@@ -9,7 +9,7 @@ const request     = require('request');
 
 
 describe('mixins after', function () {
-  it('resolve-href', function () {
+  it('resolve-href', function (done) {
     let mixin = mixinsAfter.find(m => m.id === 'resolve-href');
     let env = {
       src: 'http://example.com/test/',
@@ -18,20 +18,27 @@ describe('mixins after', function () {
           { href: '/img/foo.png' },
           { href: 'img/bar.png' },
           { href: '//example.com/baz.png' },
+          { href: 'http://example.com/test.png' },
           { href: '' }
         ]
       }
     };
 
-    mixin.fn(env, function () {
-      assert.strictEqual(env.result.snippets[0].href, 'http://example.com/img/foo.png');
-      assert.strictEqual(env.result.snippets[1].href, 'http://example.com/test/img/bar.png');
-      assert.strictEqual(env.result.snippets[2].href, 'http://example.com/baz.png');
+    mixin.fn(env, err => {
+      assert.deepStrictEqual(env.result.snippets, [
+        { href: 'http://example.com/img/foo.png' },
+        { href: 'http://example.com/test/img/bar.png' },
+        { href: 'http://example.com/baz.png' },
+        { href: 'http://example.com/test.png' },
+        { href: '' }
+      ]);
+
+      done(err);
     });
   });
 
 
-  it('ssl-force', function () {
+  it('ssl-force', function (done) {
     let mixin = mixinsAfter.find(m => m.id === 'ssl-force');
     let env = {
       result: {
@@ -42,14 +49,15 @@ describe('mixins after', function () {
       }
     };
 
-    mixin.fn(env, function () {
+    mixin.fn(env, err => {
       assert.deepStrictEqual(env.result.snippets[0].tags, [ 'ssl' ]);
       assert.deepStrictEqual(env.result.snippets[1].tags, []);
+      done(err);
     });
   });
 
 
-  it('merge', function () {
+  it('merge', function (done) {
     let mixin = mixinsAfter.find(m => m.id === 'merge');
     let env = {
       result: {
@@ -61,19 +69,40 @@ describe('mixins after', function () {
       }
     };
 
-    mixin.fn(env, function () {
+    mixin.fn(env, err => {
       assert.deepStrictEqual(env.result.snippets, [
         { href: 'http://example.com/1', tags: [ 'a', 'b', 'c', 'd' ], media: { width: 10, height: 20 } },
         { href: 'http://example.com/2', tags: [ 'd', 'e' ], media: { height: 30 } }
       ]);
+      done(err);
     });
   });
 
 
-  it('set-autoplay', function () {
+  it('set-autoplay', function (done) {
     let mixin = mixinsAfter.find(m => m.id === 'set-autoplay');
     let env = {
       config: { autoplay: true },
+      result: {
+        snippets: [
+          { href: 'http://example.com/1', tags: [ 'player' ], media: {}, type: 'text/html' },
+          { href: 'http://example.com/1', tags: [ 'player' ], media: {}, type: 'non/html' }
+        ]
+      }
+    };
+
+    mixin.fn(env, err => {
+      assert.strictEqual(env.result.snippets[0].media.autoplay, true);
+      assert.deepStrictEqual(env.result.snippets[1].media, {});
+      done(err);
+    });
+  });
+
+
+  it('set-autoplay no config', function (done) {
+    let mixin = mixinsAfter.find(m => m.id === 'set-autoplay');
+    let env = {
+      config: {},
       result: {
         snippets: [
           { href: 'http://example.com/1', tags: [ 'player' ], media: {}, type: 'text/html' }
@@ -81,24 +110,28 @@ describe('mixins after', function () {
       }
     };
 
-    mixin.fn(env, function () {
-      assert.strictEqual(env.result.snippets[0].media.autoplay, true);
+    mixin.fn(env, err => {
+      assert.deepStrictEqual(env.result.snippets[0].media, {});
+      done(err);
     });
   });
 
 
-  it('convert-str-int', function () {
+  it('convert-str-int', function (done) {
     let mixin = mixinsAfter.find(m => m.id === 'convert-str-int');
     let env = {
       result: {
         snippets: [
-          { media: { width: '10', height: '20', duration: '30', foo: '40' } }
+          { media: { width: '10', height: '20', duration: '30', foo: '40' } },
+          { media: { width: 11 } }
         ]
       }
     };
 
-    mixin.fn(env, function () {
+    mixin.fn(env, err => {
       assert.deepStrictEqual(env.result.snippets[0].media, { width: 10, height: 20, duration: 30, foo: '40' });
+      assert.deepStrictEqual(env.result.snippets[1].media, { width: 11 });
+      done(err);
     });
   });
 
@@ -143,6 +176,20 @@ describe('mixins after', function () {
     mixin.fn(env, err => {
       assert.deepStrictEqual(env.result.snippets[0].media, { width: 1, height: 1 });
       done(err);
+    });
+  });
+
+
+  it('image-size own `__loadImageSize__`', function (done) {
+    let mixin = mixinsAfter.find(m => m.id === 'image-size');
+    let env = {
+      self: { __loadImageSize__: (__, cb) => { cb('err'); } },
+      result: { snippets: [ { type: 'image', href: 'http://example.com/1.jpg' } ] }
+    };
+
+    mixin.fn(env, err => {
+      assert.strictEqual(err, 'err');
+      done();
     });
   });
 
@@ -206,12 +253,14 @@ describe('mixins after', function () {
         snippets: [
           { type: 'image', media: {}, href: 'http://example.com/1.jpg' },
           { type: 'image', href: 'http://example.com/2.png' },
-          { type: 'image', media: {}, href: 'http://example.com/2.zzz' }
+          { type: 'image', media: {}, href: 'http://example.com/2.zzz' },
+          { type: 'test', media: {}, href: 'http://example.com/3.zzz' },
+          { type: 'image', media: { width: 10, height: 20 }, href: 'http://example.com/4.zzz' }
         ]
       }
     };
 
-    mixin.fn(env, function (err) {
+    mixin.fn(env, err => {
       if (err) {
         done(err);
         return;
@@ -219,6 +268,11 @@ describe('mixins after', function () {
 
       assert.deepStrictEqual(env.result.snippets[0].media, { width: 1, height: 1 });
       assert.deepStrictEqual(env.result.snippets[1].media, { width: 1, height: 1 });
+      assert.deepStrictEqual(env.result.snippets[2].media, {});
+      assert.deepStrictEqual(env.result.snippets[3].media, {});
+      assert.deepStrictEqual(env.result.snippets[4].media, { width: 10, height: 20 });
+
+      // Check from
       server.done();
       done();
     });
@@ -229,21 +283,23 @@ describe('mixins after', function () {
     let server = nock('http://example.com')
       .head('/test/foo.bar')
       .reply(200, '', {
-        'content-type': 'foo/bar'
+        'content-type': 'text/html'
       })
       .head('/test/bar.zzz')
       .reply(200, '', {
         'content-type': ''
       });
 
+
     let mixin = mixinsAfter.find(m => m.id === 'mime-detect');
     let env = {
       self: { request },
       result: {
         snippets: [
-          { media: {}, href: 'http://example.com/test/foo.bar' },
+          { media: {}, href: 'http://example.com/test/foo.bar', tags: [] },
           { media: {}, href: 'http://example.com/test/bar.zzz' },
-          { media: {}, href: 'http://example.com/test/baz.mp4' }
+          { media: {}, href: 'http://example.com/test/baz.mp4' },
+          { media: {}, href: 'http://example.com/test/baz.mp4', type: 'old/test-type' }
         ]
       }
     };
@@ -254,9 +310,11 @@ describe('mixins after', function () {
         return;
       }
 
-      assert.strictEqual(env.result.snippets.length, 2);
-      assert.strictEqual(env.result.snippets[0].type, 'foo/bar');
+      assert.strictEqual(env.result.snippets.length, 3);
+      assert.strictEqual(env.result.snippets[0].type, 'text/html');
+      assert.deepStrictEqual(env.result.snippets[0].tags, [ 'html5' ]);
       assert.strictEqual(env.result.snippets[1].type, 'video/mp4');
+      assert.strictEqual(env.result.snippets[2].type, 'old/test-type');
       server.done();
       done();
     });
@@ -292,13 +350,13 @@ describe('mixins after', function () {
       self: { request },
       result: {
         snippets: [
-          { media: {}, href: 'badurlbadurlbadurlbadurl' }
+          { media: {}, href: 'http://' }
         ]
       }
     };
 
     mixin.fn(env, function (err) {
-      assert.strictEqual(err.message, 'Invalid URI "badurlbadurlbadurlbadurl"');
+      assert.strictEqual(err.message, 'Invalid URI "http:///"');
       done();
     });
   });
