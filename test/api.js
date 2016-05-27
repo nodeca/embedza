e78728ff97a982d42f7eaa0b088d46a33eef02ec
Kirill Efimov <kirill89@gmail.com>
2016-05-27 16:21:20 +0300

    tests: improved api coverage

--- test/api.js
+++ test/api.js
@@ -3,6 +3,7 @@
 
 const assert   = require('assert');
 const Embedza  = require('..');
+const nock     = require('nock');
 
 
 describe('API', function () {
@@ -171,28 +172,33 @@ describe('API', function () {
     });
 
 
+    it('.addDomain() disabled', function () {
+      let embedza = new Embedza({ enabledProviders: [ 'test.com', 'youtube.com' ] });
+
+      embedza.addDomain('example.com');
+
+      return embedza.info('https://example.com/asd')
+        .then(res => {
+          assert.equal(res, null);
+        });
+    });
+
+
     it('.addDomain() with options', function (done) {
       let embedza = new Embedza();
-
-      // Mock `.request`
-      embedza.request = function (url, opt, callback) {
-        if (!callback) {
-          callback = opt;
-          opt = null;
-        }
-
-        callback(null, { statusCode: 200 }, '<head>' +
-          '<meta name="twitter:image" content="https://example.com/123.jpg">' +
-          '<meta name="twitter:image:width" content="222">' +
-          '<meta name="twitter:image:height" content="333">' +
-          '</head>');
-      };
+      let server = nock('https://example.com')
+        .get('/asd')
+        .reply(200, `
+          <head>
+            <meta name="twitter:image" content="https://example.com/123.jpg">
+            <meta name="twitter:image:width" content="222">
+            <meta name="twitter:image:height" content="333">
+          </head>
+        `);
 
       embedza.addDomain({
         id: 'example.com',
-        match: [
-          /^https?:\/\/example.com\/.*/
-        ],
+        match: /^https?:\/\/example.com\/.*/,
         fetchers: [
           'meta',
           function (env, cb) {
@@ -241,8 +247,104 @@ describe('API', function () {
         assert.ok(res.mixinsExtraTest);
         assert.ok(res.mixinsAfterExtraTest);
 
+        server.done();
         done();
       });
+    });
+  });
+
+
+  describe('.info()', function () {
+    it('from cache', function () {
+      let embedza = new Embedza({
+        cache: { get: (__, cb) => { cb(null, { info: { foo: 'bar' } }); } },
+        enabledProviders: [ 'test.com' ]
+      });
+
+      return embedza.info('http://test.com/bla')
+        .then(res => {
+          assert.deepStrictEqual(res, { foo: 'bar' });
+        });
+    });
+
+
+    it('from cache with error', function () {
+      let embedza = new Embedza({
+        cache: { get: (__, cb) => { cb('err'); } },
+        enabledProviders: [ 'test.com' ]
+      });
+
+      return embedza.info('http://test.com/bla')
+        .catch(err => {
+          assert.strictEqual(err, 'err');
+        });
+    });
+
+
+    it('bad url', function () {
+      let embedza = new Embedza();
+
+      return embedza.info('badurl')
+        .then(res => {
+          assert.ok(!res);
+        });
+    });
+  });
+
+
+  describe('.render()', function () {
+    it('inline', function () {
+      let embedza = new Embedza({ enabledProviders: [ 'example.com' ] });
+      let server = nock('https://example.com')
+        .get('/asd')
+        .reply(200, '<head><meta name="title" value="test"></head>');
+
+      return embedza.render('https://example.com/asd', 'inline')
+        .then(res => {
+          assert.strictEqual(
+            res.html,
+            '<a class="ez-domain-example_com ez-inline" target="_blank" ' +
+            'href="https://example.com/asd" rel="nofollow">test</a>'
+          );
+          server.done();
+        });
+    });
+
+
+    it('not enough data', function () {
+      let embedza = new Embedza({ enabledProviders: [ 'example.com' ] });
+      let server = nock('https://example.com')
+        .get('/asd')
+        .reply(200, '');
+
+      return embedza.render('https://example.com/asd', [ 'player', 'rich', 'test' ])
+        .then(res => {
+          assert.ok(!res);
+          server.done();
+        });
+    });
+
+
+    it('inline with info', function () {
+      let embedza = new Embedza();
+
+      return embedza.render({ domain: 'a', src: 'b', meta: { title: 'c' } }, 'inline')
+        .then(res => {
+          assert.strictEqual(
+            res.html,
+            '<a class="ez-domain-a ez-inline" target="_blank" href="b" rel="nofollow">c</a>'
+          );
+        });
+    });
+
+
+    it('bar url', function () {
+      let embedza = new Embedza({ enabledProviders: [ 'badurl.badurl' ] });
+
+      return embedza.render('http://badurl.badurl/asd', 'inline')
+        .catch(err => {
+          assert.strictEqual(err.message, 'getaddrinfo ENOTFOUND badurl.badurl badurl.badurl:80');
+        });
     });
   });
 });
