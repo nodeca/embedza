16b8224b83d1f4b1909df2bfd49dc36c11c01032
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-11-30 01:16:38 +0300

    server: code cleanup

--- support/server.js
+++ support/server.js
@@ -31,56 +31,51 @@ express()
   .use(express.static(path.join(__dirname, '..', 'assets')))
   .set('view engine', 'jade')
   .set('views', path.join(__dirname, 'assets'))
-  .get('/', function (req, res) {
+  .get('/', function (req, res, next) {
     let url = req.query.url;
+    let info;
 
     let result = {
       url,
       providers
     };
 
-    if (url) {
-      embedza.info(url, (err, data) => {
-        if (err) {
-          result.err = err.toString();
-          res.render('index', result);
-          return;
-        }
-
-        if (!data) {
-          result.err = 'Can not recognize url';
-          res.render('index', result);
-          return;
-        }
-
-        embedza.render(data, 'block', (err, block) => {
-          if (err) {
-            result.err = err.toString();
-            res.render('index', result);
-            return;
-          }
-
-          embedza.render(data, 'inline', (err, inline) => {
-            if (err) {
-              result.err = err.toString();
-              res.render('index', result);
-              return;
-            }
-
-            result.json = JSON.stringify(data, null, 2);
-            result.inline = inline ? inline.html : null;
-            result.block = block ? block.html : null;
-
-            res.render('index', result);
-          });
-        });
-      });
-
+    if (!url) {
+      res.render('index', result);
       return;
     }
 
-    res.render('index', result);
+    Promise.resolve()
+      .then(() => embedza.info(url))
+      .then(data => {
+        if (!data) throw new Error('Can not recognize url');
+        info = data;
+
+        result.json = JSON.stringify(data, null, 4);
+
+        return embedza.render(info, 'block');
+      })
+      .then(block => {
+        result.block = block ? block.html : null;
+
+        return embedza.render(info, 'inline');
+      })
+      .then(inline => {
+        result.inline = inline ? inline.html : null;
+
+        res.render('index', result);
+      })
+      .catch(next);
   })
+  /* eslint-disable no-unused-vars */
+  .use(function (err, req, res, next) {
+    res.render('index', {
+      url: req.query.url,
+      providers,
+      err: err.toString()
+    });
+  })
+  /* eslint-enable no-unused-vars */
   .listen(3000, function () {
     let host = this.address().address;
     let port = this.address().port;
