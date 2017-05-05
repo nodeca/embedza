e25891b342701c874ed73892c796025d12a5ab90
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-05-05 10:27:11 +0300

    Fix rutube test

--- test/domains/rutube.ru.js
+++ test/domains/rutube.ru.js
@@ -9,9 +9,9 @@ describe('rutube.ru', function () {
   it('real request', function () {
     let embedza = new Embedza();
 
-    return embedza.info('https://rutube.ru/video/8623c112d77065f9b8167da7b7d8214f/')
+    return embedza.info('https://rutube.ru/video/c5daee24e06f0c29a5fdfd5d06b4ace6/')
       .then(res => {
-        assert.strictEqual(res.meta.title, 'Физрук: Жил-был пёс');
+        assert.strictEqual(res.meta.title, 'Танцы, 3 сезон, 22 серия ФИНАЛ');
         res.snippets.forEach(s => {
           if (s.tags.indexOf('player') !== -1) assert.strictEqual(s.media.autoplay, 'autoStart=true');
         });
