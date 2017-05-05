428186ce8c62f78687167f35fab323fb313d5835
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-05-05 12:09:09 +0300

    Cleanup: drop outdated static demo content

--- demo/player.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', '.ez-player-placeholder', function (event) {
-  if (event.target.nodeName === 'A') {
-    return;
-  }
-
-  var $el = $(this);
-
-  $el.replaceWith($el.closest('.ez-player').data('placeholder'));
-});
